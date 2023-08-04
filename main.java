12f281b68413e9e3ef1c429d959e694e0e820115
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-08-04 21:08:13 +0530

    move the spacecraft forward/backward

---
+++ main.java
@@ -0,0 +1,67 @@
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+class Spacecraft {
+    private int x;
+    private int y;
+    private int z;
+    private char direction; // N, S, E, W, U, D
+
+    public Spacecraft(int x, int y, int z, char direction) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = direction;
+    }
+
+    // Add getters for x, y, z, and direction if needed
+
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ") - " + direction;
+    }
+
+    public void move(char command) {
+        switch (direction) {
+            case 'N':
+                if (command == 'f')
+                    y++;
+                else if (command == 'b')
+                    y--;
+                break;
+            case 'S':
+                if (command == 'f')
+                    y--;
+                else if (command == 'b')
+                    y++;
+                break;
+            case 'E':
+                if (command == 'f')
+                    x++;
+                else if (command == 'b')
+                    x--;
+                break;
+            case 'W':
+                if (command == 'f')
+                    x--;
+                else if (command == 'b')
+                    x++;
+                break;
+            case 'U':
+                if (command == 'f')
+                    z--;
+                else if (command == 'b')
+                    z++;
+                break;
+            case 'D':
+                if (command == 'f')
+                    z++;
+                else if (command == 'b')
+                    z--;
+                break;
+        }
+    }
+}
